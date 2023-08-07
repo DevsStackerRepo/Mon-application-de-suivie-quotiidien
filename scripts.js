0919ff65a3bf659810ce73f141a2d6f26cc209a9
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-07 11:43:42 +0000

    creation de bouton de suppression dans le dom

--- scripts.js
+++ scripts.js
@@ -6,12 +6,23 @@ function inputLength(){
     return input.value.length;
 }
 
-function createElement(){
-    let li = document.createElement("li");
-    li.innerHTML = input.value + "<br /> <button>Modifier</button>       <button>Supprimer</button>";
-    showTodo.appendChild(li);
-    input.value="";
-    }
+// function createElement(){
+//     let li = document.createElement("li");
+//     li.innerHTML = input.value + "<br /> <button>Modifier</button>       <button>Supprimer</button>";
+//     showTodo.appendChild(li);
+//     input.value="";
+//     }
+
+    function createElement(){
+        let li = document.createElement("li");
+        let deleteButton = document.createElement("button");
+        deleteButton.innerText = `supprimer ${input.value}`;
+        li.innerHTML = input.value;
+        li.appendChild(deleteButton);
+        showTodo.appendChild(li);
+        input.value="";
+        }
+    
 
     function addListAfterClick(){
         if(inputLength() > 0){
