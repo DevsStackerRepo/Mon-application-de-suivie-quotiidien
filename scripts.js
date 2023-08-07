058e7bd040b63ae351d78d6ee1136dcf46b52dd5
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-07 11:30:33 +0000

    le premier commit de mon projet

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let input = document.getElementById("userInput");
+let enter = document.getElementById("enter")
+let showTodo = document.getElementById("show_todolist");
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createElement(){
+    let li = document.createElement("li");
+    li.innerHTML = input.value + "<br /> <button>Modifier</button>       <button>Supprimer</button>";
+    showTodo.appendChild(li);
+    input.value="";
+    }
+
+    function addListAfterClick(){
+        if(inputLength() > 0){
+            createElement();
+        }
+    }
+
+    function addListAfterKeypress(e){
+        if(inputLength() > 0 && e.which === 13 ){
+            createElement();
+        }
+    }
+
+    enter.addEventListener("click",addListAfterClick)
+    input.addEventListener("keypress",addListAfterKeypress)
+
+
