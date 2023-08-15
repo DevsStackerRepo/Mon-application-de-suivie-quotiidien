65f9889c2f66a73751d214bae6157102b73cf674
Bams Muki <ibrahimfofana.mutaki0107@gmail.com>
2023-08-15 21:47:00 +0000

    Creation et Insertion du bouton editer dans le Dom

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 let input = document.getElementById("userInput");
-let enter = document.getElementById("enter")
+let enter = document.getElementById("enter");
 let showTodo = document.getElementById("show_todolist");
 
-function inputLength(){
-    return input.value.length;
+function inputLength() {
+  return input.value.length;
 }
 
 // function createElement(){
@@ -13,36 +13,39 @@ function inputLength(){
 //     input.value="";
 //     }
 
+function createElement() {
+  let li = document.createElement("li");
+  li.innerHTML = input.value;
 
-function createElement(){
-    let li = document.createElement("li");
-    let deleteButton = document.createElement("button");
-        deleteButton.innerHTML =  `supprimer ${input.value}`;
-        deleteButton.onclick = () => del(li);
-        li.innerHTML = input.value;
-        li.appendChild(deleteButton);
-        showTodo.appendChild(li);
-        input.value="";
-        }
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `supprimer ${input.value}`;
+  deleteButton.onclick = () => del(li);
 
-        function del(element){
-            element.remove();
-        }
-    
+  let editButton = document.createElement("button");
+  editButton.innerHTML = `editer ${input.value}`;
 
-    function addListAfterClick(){
-        if(inputLength() > 0){
-            createElement();
-        }
-    }
+  li.appendChild(deleteButton);
+  li.appendChild(editButton);
 
-    function addListAfterKeypress(e){
-        if(inputLength() > 0 && e.which === 13 ){
-            createElement();
-        }
-    }
+  showTodo.appendChild(li);
+  input.value = "";
+}
 
-    enter.addEventListener("click",addListAfterClick)
-    input.addEventListener("keypress",addListAfterKeypress)
+function del(element) {
+  element.remove();
+}
 
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createElement();
+  }
+}
 
+function addListAfterKeypress(e) {
+  if (inputLength() > 0 && e.which === 13) {
+    createElement();
+  }
+}
+
+enter.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
